5703aaa2fce371dde9ddee5180746de2b38647e9
Ivan <Sgoidxxl@gmail.com>
2018-03-20 10:42:15 +0300

    add port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as express from "express";
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get("*", (req, res) => {
   res.send("Hello World!");
 });
+
+app.listen(PORT);
